a6716ee15c9a763903d41837c6bc8320bcf73227
julio <julior0518@gmail.com>
2022-02-13 17:13:46 -0500

    add font style

--- client/src/components/RoleCard.jsx
+++ client/src/components/RoleCard.jsx
@@ -5,19 +5,26 @@ import '../style/roleCard.css'
 
 function RoleCard({roles, current}) {
     let empty = roles.esteRoleMoviesId.length === 0
-
-
+    const [select, setSelect ] = useState(0)
+    console.log(select === 0)
     return (
         <div className= "RoleCard">
+            <div className='cardsLeft'>
                 {roles.esteRoleMoviesId.map((role, i) => {
                     return (
-                        <ul>
-                            <div className='card'>
-                                <p>{ roles.esteRoleMoviesId[i].name}</p>
+                            <div className= {select === i ? "cardOuter": null}>
+                                <div className='card's onClick={()=>{setSelect(i)}}>
+                                    <h4>{ role.name} - { role.age} -{ role.ethnicity}</h4>
+                                    <p>{ role.description}</p>
+                                    <p>{ role.category}</p>
+                                </div>
                             </div>
-                        </ul>
                     )
                 })}
+            </div>
+            <div className="cardsRight">
+                asdf
+            </div>
         </div>
     );
     }
