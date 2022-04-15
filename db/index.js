a18a7f416a657dc979541b5c616c492fdf6399a0
julio <julior0518@gmail.com>
2022-04-15 12:11:28 -0400

    Revert "Revert "Revert " adjustment"""
    This reverts commit b43d88d05806a32ae4b8b7b4a913ae2f8a889993.

--- db/index.js
+++ db/index.js
@@ -13,3 +13,4 @@ mongoose
 
 const db = mongoose.connection;
 
+module.exports = db;
--- server.js
+++ server.js
@@ -20,7 +20,8 @@ db.on('error', console.error.bind(console, 'MongoDB connection error 1:'));
 
 
 if(process.env.NODE_ENV === 'production'){    
-    app.use(express.static('client/build'))  
+    app.use(express.static('client/build'))  // set static folder 
+    //returning frontend for any route other than api 
     app.get('*',(req,res)=>{     
         res.sendFile (path.resolve(__dirname,'frontend','build','index.html' ));    
     });
