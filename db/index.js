0e5b2138addd002be1b41abdd1f8a3444bb59b10
julio <julior0518@gmail.com>
2022-04-15 11:19:43 -0400

    ready to deploy

--- db/index.js
+++ db/index.js
@@ -1,16 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+require('dotenv').config() 
 
-let MONGODB_URI = 'mongodb://127.0.0.1:27017/spotlight_3';
+let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/spotlight_3'
 
 mongoose
-    .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+    .connect(dbUrl, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useFindAndModify: true
+    })
     .then(() => {
-        console.log('Successfully connected to MongoDB.');
+        console.log('Successfully connected to MongoDB.')
     })
     .catch((e) => {
-        console.error('Connection error', e.message);
-    });
+        console.error('Connection error', e.message)
+    })
+mongoose.set('debug', true)
+const db = mongoose.connection
 
-const db = mongoose.connection;
-
-module.exports = db;
+module.exports = db
--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const routes = require('./routes');
 const db = require('./db');
 const logger = require('morgan');
-
+const path = require('path')
+require("dotenv").config()
+const cors = require("cors");
 
 /////// require() imports and middleware here ^ ///////
 
@@ -12,10 +14,21 @@ const app = express();
 app.use(express.urlencoded({extended: true, limit: '100mb'}));
 app.use(express.json({limit: '100mb'}));
 app.use(logger('dev')); 
-app.use('/', routes);
+app.use('/api', routes);
+app.use(cors());
 
 
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+    })
+}
+
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
